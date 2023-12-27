0287c19a4707b9e4cd98e430ae82123c6f27aac3
Oscar Sangrador <oscar@vivla.com>
2023-12-27 17:49:38 +0100

    styles: some navbar styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import logoVivla from '../../assets/logo-vivla.jpeg'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import logoVivla from '../../assets/logo-vivla.jpeg';
+import './navbar.css';
+
 
 function Navbar() {
     return (
-        <nav>
+        <nav className='navbar'>
             <img src={logoVivla} alt="vivla name" />
 
             <Link to="/homes"> Homes</Link>
