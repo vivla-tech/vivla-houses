d99f0e13e68c956ced84cfcad200c893fd7b0ab1
Oscar Sangrador <oscar@vivla.com>
2024-01-16 17:12:23 +0100

    styles: add background, grids and some styles to home cards

--- src/components/Homes/Homes.jsx
+++ src/components/Homes/Homes.jsx
@@ -1,20 +1,38 @@
 import useHomes from "../../hooks/useHomes";
+import './homes.css';
 
 function Homes() {
   const homes = useHomes()
 
-  console.log(homes)
+  // console.log(homes)
   return (
     <>
       {homes?.map((mapHomes) => (
-        <p key={mapHomes.id}>
-          {mapHomes.homeName}
-          <img src={mapHomes.urlImages} alt={`${mapHomes.homeName} home`} />
-          <p>{mapHomes.hub}</p>
-          <p>{mapHomes.market}</p>
-          <p>{mapHomes.address}</p>
-          <p>{mapHomes.coordinates}</p>
-        </p>
+        <section className="home-list"
+          key={mapHomes.id}>
+          <div>
+            <h2 className="home-name">
+              {mapHomes.homeName} - 150.000€
+            </h2>
+            <img src={mapHomes.urlImages} alt={`${mapHomes.homeName} home`} />
+          </div>
+          <div className="home-info">
+            <ul className="home-details">
+              <li>
+                <p>{mapHomes.hub}</p>
+              </li>
+              <li>
+                <p>{mapHomes.market}</p>
+              </li>
+              <li>
+                <p>{mapHomes.address}</p>
+              </li>
+              <li>
+                <p>{mapHomes.coordinates}</p>
+              </li>
+            </ul>
+          </div>
+        </section>
       ))}
     </>
   )
