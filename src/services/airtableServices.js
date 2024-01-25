ef425fc05477167d7ebcacd208634bb84bc68260
Oscar Sangrador <oscar@vivla.com>
2024-01-25 17:44:42 +0100

    fix: problem with updated amenities

--- src/services/airtableServices.js
+++ src/services/airtableServices.js
@@ -3,10 +3,6 @@ import axios from "axios";
 
 const { VITE_AIRTABLE_TOKEN, VITE_AIRTABLE_BASE } = import.meta.env;
 
-// export const airtableBase = new Airtable({
-//     apiKey: VITE_AIRTABLE_TOKEN,
-// })
-//     .base(VITE_AIRTABLE_BASE)
 
 export const axiosInstance = axios.create({
     baseURL: `https://api.airtable.com/v0/${VITE_AIRTABLE_BASE}`,
@@ -47,37 +43,6 @@ export const updateHomeData = async (id, updatedFields) => {
     }
 };
 
-// export const getHomesData = async () => {
-//     try {
-//         const data = await airtableBase('homes')
-//             .select({ view: 'Grid view' })
-//             .all();
-//         return data;
-//     } catch (e) {
-//         return console.error(e);
-//     }
-// }
-
-
-// export const removeHomesData = async (id) => {
-//     try {
-//         await airtableBase('homes').destroy([id])
-//     } catch (error) {
-//         console.error('error to remove home data', error)
-//     }
-// }
-
-// export const updateHomeData = async (id, updatedFields) => {
-//     try {
-//         const updatedRecord = await airtableBase('homes').update([{
-//             id: id,
-//             fields: updatedFields
-//         }]);
-//         return updatedRecord;
-//     } catch (error) {
-//         console.error('error updating home data', error);
-//     }
-// }
 
 
 
